0e4d85f5636a2d6b5121fda57e203078e6df886a
Sam Yedigaryan <yedigaryansem@gmail.com>
2021-04-09 22:33:23 +0400

    task/gorest.co.in

---
+++ src/test/java/rest/gorestCoIn/utils/User.java
@@ -0,0 +1,36 @@
+package rest.gorestCoIn.utils;
+
+import io.restassured.path.json.JsonPath;
+
+public class User {
+    private String name;
+    private String email;
+    private String gender;
+    private String status;
+
+    public User(String body){
+
+        JsonPath jsonPath = new JsonPath(body);
+
+        this.name = jsonPath.get("name");
+        this.email = jsonPath.get("email");
+        this.gender = jsonPath.get("gender");
+        this.status = jsonPath.get("status");
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
